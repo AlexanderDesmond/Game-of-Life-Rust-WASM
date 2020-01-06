2f29a32072732daabfdb10d987014df11837e827
Alex <AlexanderDesmond848898@gmail.com>
2020-01-06 15:02:30 +1100

    Implement life.

--- www/index.js
+++ www/index.js
@@ -1,3 +1,16 @@
-import * as wasm from "wasm-game-of-life";
+import { Universe } from "wasm-game-of-life";
 
-wasm.greet("Alex");
+// Get canvas element.
+const pre = document.getElementById("game-of-life-canvas");
+
+// Instantiate the universe.
+const universe = Universe.new();
+
+const renderLoop = () => {
+  pre.textContent = universe.render();
+  universe.tick();
+
+  requestAnimationFrame(renderLoop);
+};
+
+requestAnimationFrame(renderLoop);
